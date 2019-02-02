671add7a21ab6f03a27efcbf6f87c2b343a66874
salvi205 <salvi205s@gmail.com>
2019-02-02 08:56:05 +0100

    Cliente con atributos, constructor, getters y setters creados

--- src/emresaAlquilerVehiculos/Vehiculo.java
+++ src/emresaAlquilerVehiculos/Vehiculo.java
@@ -10,5 +10,74 @@ package emresaAlquilerVehiculos;
  * @author Salva
  */
 public class Vehiculo {
-    
+
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private double tarifa;
+    private boolean disponible;
+
+    public Vehiculo(String matricula, String marca, String modelo, String color, double tarifa, boolean disponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return matricula + "\t" + marca + " " + modelo + "\t" + color + "\t" + tarifa + "\t" + disponible;
+    }
+
 }
