bbad829f45a41c4eb00c83a8b68610c29a92a1bd
salvi205 <salvi205s@gmail.com>
2019-02-02 08:45:44 +0100

    Cliente con atributos, constructor, getters y setters creados

--- src/emresaAlquilerVehiculos/Cliente.java
+++ src/emresaAlquilerVehiculos/Cliente.java
@@ -10,5 +10,43 @@ package emresaAlquilerVehiculos;
  * @author Salva
  */
 public class Cliente {
-    
+
+    private String nif;
+    private String nombre;
+    private String apellidos;
+
+    public Cliente(String nif, String nombre, String apellidos) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return nif + " \t" + apellidos + ",\t" + nombre;
+    }
 }
