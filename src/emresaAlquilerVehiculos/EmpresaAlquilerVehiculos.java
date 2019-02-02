c6b58ee1101333ff30dd4f6ee682117e671a6dbf
salvi205 <salvi205s@gmail.com>
2019-02-02 09:04:00 +0100

    Atributos y constructor creados

--- src/emresaAlquilerVehiculos/EmpresaAlquilerVehiculos.java
+++ src/emresaAlquilerVehiculos/EmpresaAlquilerVehiculos.java
@@ -11,5 +11,41 @@ package emresaAlquilerVehiculos;
  * @author Salva
  */
 public class EmpresaAlquilerVehiculos {
-    
+
+    // Atributos de la empresa
+    private String cif;
+    private String nombre;
+    private String paginaWeb;
+
+    /* Atributos para controlar el total de clientes que tiene la
+    empresa y array de almacenamiento para los objetos Cliente */
+    private int totalClientes;
+    private Cliente[] clientes;
+
+    /* Atributos para controlar el total de vehiculos disponibles en
+    alquiler que tiene la empresa y array de almacenamiento para los objetos
+    Vehiculo */
+    private int totalVehiculos;
+    private Vehiculo[] vehiculos;
+
+    /* Atributos para controlar el histórico de alquileres: total de
+    alquileres realizados y array de almacenamiento para los objetos
+    vehiculoAlquilado*/
+    private int totalAlquileres;
+    private VehiculoAlquilado[] alquileres;
+
+    public EmpresaAlquilerVehiculos(String cif, String nombre, String paginaWeb) {
+        this.cif = cif;
+        this.nombre = nombre;
+        this.paginaWeb = paginaWeb;
+        this.totalClientes = 0;
+
+        //no hay clientes creados
+        this.clientes = new Cliente[50]; //apunta a null
+        this.totalVehiculos = 0;
+        this.vehiculos = new Vehiculo[50]; //apunta a null
+        this.totalAlquileres = 0;
+        this.alquileres = new VehiculoAlquilado[100]; //apunta a null
+
+    }
 }
