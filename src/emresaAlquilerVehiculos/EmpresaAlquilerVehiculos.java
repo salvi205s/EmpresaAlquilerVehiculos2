e1f1b093e000916152f94af046ec4782d8e7ed3a
salvi205 <salvi205s@gmail.com>
2019-02-02 09:05:54 +0100

    Getters y setters y toString creados

--- src/emresaAlquilerVehiculos/EmpresaAlquilerVehiculos.java
+++ src/emresaAlquilerVehiculos/EmpresaAlquilerVehiculos.java
@@ -48,4 +48,76 @@ public class EmpresaAlquilerVehiculos {
         this.alquileres = new VehiculoAlquilado[100]; //apunta a null
 
     }
+    
+     public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
+    public int getTotalClientes() {
+        return totalClientes;
+    }
+
+    public void setTotalClientes(int totalClientes) {
+        this.totalClientes = totalClientes;
+    }
+
+    public Cliente[] getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Cliente[] clientes) {
+        this.clientes = clientes;
+    }
+
+    public int getTotalVehiculos() {
+        return totalVehiculos;
+    }
+
+    public void setTotalVehiculos(int totalVehiculos) {
+        this.totalVehiculos = totalVehiculos;
+    }
+
+    public Vehiculo[] getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(Vehiculo[] vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public int getTotalAlquileres() {
+        return totalAlquileres;
+    }
+
+    public void setTotalAlquileres(int totalAlquileres) {
+        this.totalAlquileres = totalAlquileres;
+    }
+
+    public VehiculoAlquilado[] getAlquileres() {
+        return alquileres;
+    }
+
+    public void setAlquileres(VehiculoAlquilado[] alquileres) {
+        this.alquileres = alquileres;
+    }
 }
