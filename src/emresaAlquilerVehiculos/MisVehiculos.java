d07919a00d04cc521d588a4815c38f73b0e6bbf9
salvi205 <salvi205s@gmail.com>
2019-02-02 09:43:49 +0100

    Clientes creados eimprime clientes y vehiculos

--- src/emresaAlquilerVehiculos/MisVehiculos.java
+++ src/emresaAlquilerVehiculos/MisVehiculos.java
@@ -15,7 +15,32 @@ public class MisVehiculos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EmpresaAlquilerVehiculos easydrive = new EmpresaAlquilerVehiculos("A-28-187189", "easy drive", "www.easydrive.com");
+        
+        //Registro los clientes de la empresa
+        easydrive.registrarCliente(new Cliente("X5618927C", "Juan", "González López"));
+
+        easydrive.registrarCliente(new Cliente("Z7568991Y", "Luis", "Fernández Gómez"));
+
+        // registro de los vehículos de la empresa
+        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda", "Fabia", "Blanco", 90.0, false));
+
+        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang", "Rojo", 150.0, true));
+
+        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
+
+        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza", "Blanco", 90.0, false));
+
+        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato", "Azul", 80.0, true));
+        
+        
+        // imprime la relación de clientes de easydrive
+        easydrive.imprimirClientes();
+
+        System.out.println("----------------------------------------------------------------------");
+
+        // imprime el catálogo de vehículos de easydrive
+        easydrive.imprimirVehiculos();
     }
     
 }
