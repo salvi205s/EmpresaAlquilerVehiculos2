4abd13f3af6cd7fb4a8acf6a6cd3fd20b5a005d4
salvi205 <salvi205s@gmail.com>
2019-02-11 16:15:55 +0100

    Metodo para imprimir clientes aleatorios

--- src/emresaAlquilerVehiculos/Cliente.java
+++ src/emresaAlquilerVehiculos/Cliente.java
@@ -5,6 +5,8 @@
  */
 package emresaAlquilerVehiculos;
 
+import java.util.Random;
+
 /**
  *
  * @author Salva
@@ -19,6 +21,42 @@ public class Cliente {
         this.nif = nif;
         this.nombre = nombre;
         this.apellidos = apellidos;
+    }
+
+    public static void clienteAleatorio() {
+        Random aleatorio = new Random();
+
+        String[] nombre = {"Pepe", "Manolo ", "Maria ", "Ana", "Carol", "Rocio ", "Jesus ", "Diego"};
+        String[] apellidos = {"Gomez", "Perez ", "hernander ", "Benitez", "Cazorla", "Molina ", "Santos ", "Gonzalez"};
+        String[] apellidos2 = {"Gomez", "Perez ", "hernander ", "Benitez", "Cazorla", "Molina ", "Santos ", "Gonzalez"};
+
+        String[] abecedario = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+        int m1 = aleatorio.nextInt(9);
+        int m2 = aleatorio.nextInt(9);
+        int m3 = aleatorio.nextInt(9);
+        int m4 = aleatorio.nextInt(9);
+        int m5 = aleatorio.nextInt(9);
+        int m6 = aleatorio.nextInt(9);
+        int m7 = aleatorio.nextInt(9);
+        int m8 = aleatorio.nextInt(9);
+        int m9 = aleatorio.nextInt(9);
+
+        String numDni = m1 + "" + m2 + "" + m3 + "" + m4 + "" + m5 + "" + m6 + "" + m7 + "" + m8 + "" + m9;
+
+        int letraAlea = aleatorio.nextInt(abecedario.length);
+
+        String letra1 = abecedario[letraAlea];
+
+        String dni = numDni + " " + letra1;
+
+        int nomAlea = aleatorio.nextInt(nombre.length);
+        int apell1Alea = aleatorio.nextInt(apellidos.length);
+        int apell2Alea = aleatorio.nextInt(apellidos2.length);
+
+        String cliente = "Dni cliente: " + dni + "\tNombre: " + nombre[nomAlea] + "\tApellidos: " + apellidos[apell1Alea] + " " + apellidos2[apell2Alea];
+
+        System.out.println(cliente);
     }
 
     public String getNif() {
